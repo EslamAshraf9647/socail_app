39718c193e985c583438844f26b75291819f1425
Eslam Ashraf <eslamelmawazen300@gmail.com>
2025-03-03 12:29:47 +0200

    change log

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ const bootStrap =async() => {
 
 
     const port =3000
-    app.listen(port,()=> console.log('server is running on port'+port))
+    app.listen(port,()=> console.log('Server is Running on port'+port))
 
 }
 export default bootStrap
